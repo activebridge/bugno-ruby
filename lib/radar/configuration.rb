0bc884e31e1457388372959dfa0c3b861840e6bd
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-04-02 15:37:53 +0300

    Initial commit, add configuration.

---
+++ lib/radar/configuration.rb
@@ -0,0 +1,9 @@
+module Radar
+  class Configuration
+    attr_accessor :api_key
+
+    def initialize
+      @api_key = nil
+    end
+  end
+end
---
+++ lib/radar/version.rb
@@ -0,0 +1,3 @@
+module Radar
+  VERSION = "0.1.0"
+end
