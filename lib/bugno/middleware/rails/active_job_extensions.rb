69ec214936e2398b3a0d07ac69adf9607d5471a0
Dmitry Sivoglaz <grayeyed16@gmail.com>
2019-12-08 16:30:45 +0200

    Remove supported_by_specific_integration? method from active_job_extenstions

--- lib/bugno/middleware/rails/active_job_extensions.rb
+++ lib/bugno/middleware/rails/active_job_extensions.rb
@@ -16,19 +16,14 @@ module Bugno
 
         def capture_and_reraise(job, block)
           block.call
+        rescue Error
+          raise
         rescue Exception => e
           Handler.call(exception: e, job: job_data(job)) if Bugno.configured?
           raise e
         end
 
-        def supported_by_specific_integration?(job)
-          return ADAPTERS.include?(job.class.queue_adapter.to_s) if ::Rails.version.to_f < 5.0
-
-          ADAPTERS.include?(job.class.queue_adapter.class.to_s)
-        end
-
         def job_data(job)
-          return {} if supported_by_specific_integration?(job)
           data = {
             active_job: job.class.name,
             arguments: job.arguments,
